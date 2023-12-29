17e30a6d4fa85cf49670640b7809ec0ffc828bf8
noman5945 <abdullahnoman.5945@gmail.com>
2023-12-29 20:04:12 +0600

    modals not working

---
+++ src/components/Modals/AllContact.jsx
@@ -0,0 +1,24 @@
+import { Modal } from "bootstrap";
+import React from "react";
+
+const AllContact = (props) => {
+  const { show, handleClose } = props;
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>body</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default AllContact;
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import AllContact from "./Modals/AllContact";
 
 const Problem2 = () => {
+  const [show, setShow] = useState(true);
+  const handleShow = () => {
+    setShow(true);
+  };
+  const handleClose = () => {
+    setShow(false);
+  };
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
 
-    return (
-
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+        <div className="d-flex justify-content-center gap-3">
+          <button
+            className="btn btn-lg btn-outline-primary"
+            type="button"
+            onClick={handleShow}
+          >
+            All Contacts
+          </button>
+          <button className="btn btn-lg btn-outline-warning" type="button">
+            US Contacts
+          </button>
+          {/* <AllContact show={show} handleClose={handleClose}></AllContact> */}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
